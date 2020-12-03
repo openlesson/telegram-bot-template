7f0b3406aceaa1d4fc2ca7c1f49a91f331f1ab13
Max Shaptala <maxshaptala@gmail.com>
2020-12-03 17:21:55 +0200

    Init repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# Template to create telegram bot
+
---
+++ src/main/java/org/itstep/App.java
@@ -0,0 +1,7 @@
+package org.itstep;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Write your bot here");
+    }
+}
